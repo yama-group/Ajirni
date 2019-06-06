2aac592da49cbc309cdbc2eb096b8c4749590716
Yasser <eng.yasserkassem@gmail.com>
2019-06-06 16:54:09 +0300

    rud api's fro item done

--- yama/ajirni/MobClient/urls.py
+++ yama/ajirni/MobClient/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateItem
+from .views import ItemsRud, ItemsList, CreateItem
 
 
 urlpatterns = [
-    path('add/', CreateItem.as_view(), name="add")
+    path('additem/', CreateItem.as_view(), name="add"),
+    path('rud/<int:pk>', ItemsRud.as_view(), name="rud"),
+    path('all/', ItemsList.as_view(), name="list")
 ]
