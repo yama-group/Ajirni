447c624e40ea1c8978da2f9d1df74f8d156d1b0b
engaymanh <eng.aymanhariri@gmail.com>
2019-06-13 13:39:24 +0300

    add api s

--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -1,72 +1,72 @@
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-// import { createPost } from '../actions/postActions';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
-// class PostForm extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       email: '',
-//       password: ''
-//     };
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
 
-//     this.onChange = this.onChange.bind(this);
-//     this.onSubmit = this.onSubmit.bind(this);
-//   }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-//   onChange(e) {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-//   onSubmit(e) {
-//     e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-//     const logIn = {
-//       email: this.state.email,
-//       password: this.state.password
-//     };
+    const logIn = {
+      email: this.state.email,
+      password: this.state.password
+    };
 
-//     this.props.signIn(logIn);
-//   }
+    this.props.signIn(logIn);
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Sign In</h1>
-//         <div className="register-area ptb-100">
-//                 <div className="container">
-//                     <div className="row">
-//                         <div className="col-md-12 col-12 col-lg-6 col-xl-6 ml-auto mr-auto">
-//                             <div className="login">
-//                                 <div className="login-form-container">
-//                                     <div className="login-form">
-//                                         <form action="#" method="post">
-//                                             <input type="text" name="user-name" placeholder="Username"/>>
-//                                             <input type="password" name="user-password" placeholder="Password"/>>
-//                                             <div className="button-box">
-//                                                 <div className="login-toggle-btn">
-//                                                     <input type="checkbox"/>>
-//                                                     <label>Remember me</label>
-//                                                     {/* <a>Forgot Password?</a> */}
-//                                                 </div>
-//                                                 <button type="submit" className="default-btn floatright">Login</button>
-//                                             </div>
-//                                         </form>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h1>Sign In</h1>
+        <div className="register-area ptb-100">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-12 col-lg-6 col-xl-6 ml-auto mr-auto">
+                            <div className="login">
+                                <div className="login-form-container">
+                                    <div className="login-form">
+                                        <form action="#" method="post">
+                                            <input type="text" name="user-name" placeholder="Username"/>>
+                                            <input type="password" name="user-password" placeholder="Password"/>>
+                                            <div className="button-box">
+                                                <div className="login-toggle-btn">
+                                                    <input type="checkbox"/>>
+                                                    <label>Remember me</label>
+                                                    {/* <a>Forgot Password?</a> */}
+                                                </div>
+                                                <button type="submit" className="default-btn floatright">Login</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>
+    );
+  }
+}
 
-// PostForm.propTypes = {
-//   createPost: PropTypes.func.isRequired
-// };
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
 
-// export default connect(null, { createPost })(PostForm);
+export default connect(null, { createPost })(PostForm);
