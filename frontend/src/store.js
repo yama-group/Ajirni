13cb121328e62e7db35d0307dc72a56dbe87ff0c
mohamedartan <moha.abdi1771@gmail.com>
2019-06-27 16:29:46 +0300

    test

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,11 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ||
-  compose(
+  composeEnhancers(
     applyMiddleware(...middleware)
   )
 );
--- frontend/tests/spec.js
+++ frontend/tests/spec.js
@@ -19,12 +19,16 @@
 // }
 
 
+
+
+import moxios from 'moxios';
+import tstore from '../src/store'
+import { testStore } from '../Utils'
+import { getAllUsers } from '../src/actions/usersAct'
 import usersReducer from '../src/reducers/usersReducer'
 import itemsReducer from '../src/reducers/itemsReducer'
-import * as actions from '../src/actions/usersAct'
+//import * as actions from '../src/actions/usersAct'
 import * as types from '../src/actions/types'
-
-
 
 
 describe('users reducer', () => {
@@ -56,10 +60,89 @@ describe('items reducer', () => {
     }
     expect(itemsReducer(undefined, {})).toEqual(expectedAction)
   })
-
-
+  it('should handle items', () => {
+    expect(
+      itemsReducer([], {
+        type: types.FETCH_ITEMS,
+        items: [],
+        item: {},
+        item_id: ""
+      })
+    ).toEqual({ items: undefined, item: undefined, item_id: undefined })
+  })
 
 })
+
+
+
+
+
+
+describe('fetches all users', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  test('store is updated correctly', () => {
+    const expectedState = [{
+      email: "moh@gmail.com",
+      first_name: "moha",
+      id: 10,
+      image_url: "img",
+      last_name: "moha",
+      phone: "35423",
+      username: "moha1"
+    },
+    {
+      email: "example@gmail.com",
+      first_name: "exam",
+      id: 10,
+      image_url: "img",
+      last_name: "exam",
+      phone: "35423",
+      username: "exam"
+    }]
+    const store = testStore;
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: expectedState
+      })
+    })
+
+    // return (dispatch) => {
+    //   return request.then((data) => {
+    //     dispatch({ type: types.FETCH_USERS, payload: data });
+    //   });
+    // }
+
+    return store.dispatch(getAllUsers()).then(() => {
+      const newState = store.getState();
+      expect(newState.usersReducer).toBe(expectedState)
+    })
+  })
+  // it('should call a getAllUsers function', () => {
+  //   getAllUsers()("/allusers/", {}).then(response => {
+  //     expect(response).toEqual({
+  //       data: {},
+  //     });
+  //   });
+  // })
+
+  //axios.js file
+  // export default {
+  //   get: jest.fn(() => Promise.resolve({ data: {} }))
+  // }
+
+})
+
+
 
 
 
